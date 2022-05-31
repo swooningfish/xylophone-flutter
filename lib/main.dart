1fd7f2fbcb8cc2b619391c6d1d06b3bb56ea6b86
Greg Colley <greg.colley@gmail.com>
2022-05-31 15:46:05 +0100

    Play sounds with function and param

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,29 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-              child: TextButton(
+          child: Column(
+            children: <Widget>[
+              ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                  ),
                   onPressed: () {
-                    final player = AudioCache();
-                    player.play('note1.wav');
+                    playSound(1);
                   },
-                  child: Text('Click Me'))),
+                  child: Text('Click Me')),
+            ],
+          ),
         ),
       ),
     );
